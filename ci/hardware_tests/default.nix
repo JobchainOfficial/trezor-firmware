cbb56bdbcdf010feec1500fbc674ff7e3c748b4f
Tomas Susanka <tsusanka@gmail.com>
2020-04-04 21:10:07 +0200

    x

--- ci/hardware_tests/default.nix
+++ ci/hardware_tests/default.nix
@@ -1,7 +1,4 @@
-with import (fetchTarball {
-  url = "https://github.com/nixos/nixpkgs-channels/archive/c4adeddb5f8e945517068968d06ea838b7c24bd3.tar.gz";
-  sha256 = "1vpm73y7d0j2cviq0cgjwdj64h2v0c349capiyqf5f6071anx7d7";
-}) { };
+with import <nixpkgs> {};
 
 stdenv.mkDerivation rec {
   name = "trezor-firmware-hardware-tests";
